ac5b9b8ae35cb2a9894f94b59d179e1c9e9ccf65
Earl831SC <ryanjudd31@gmail.com>
2015-06-26 10:33:55 -0700

    initial commit

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+describe "Static pages" do
+	let(:base_title) { "Ruby on Rails Tutorial Sample App"}
+	subject { page }
+	shared_examples_for "All Static Pages" do
+		it { should have_selector 'h1', text: heading }
+		it {should have_selector 'title', text: full_title(page_title) }
+	end
+	describe "Home page" do
+		before { visit root_path }
+		let(:heading) { 'Sample App' }
+		let(:page_title) { '' }
+		it_should_behave_like "All Static Pages"
+		it { should_not have_selector 'title', text: " | Home" }
+	end
+	describe "Help page" do
+		before { visit help_path }
+		let(:heading){ 'Help' }
+		let(:page_title) { 'Help'}
+		it_should_behave_like "All Static Pages"
+	end
+	describe "About page" do
+		before { visit about_path }
+		let(:heading) { 'About Us' }
+		let(:page_title) { 'About Us' }
+		it_should_behave_like "All Static Pages"
+	end
+	describe "Contact page" do
+		before { visit contact_path }
+		let(:heading) { 'Contact' }
+		let(:page_title) { 'Contact'}
+		it_should_behave_like "All Static Pages"
+	end 
+	it 'should have the right links on the layout' do
+		visit root_path
+		click_link "About"
+		page.should have_selector 'title', text: full_title('About Us') 
+		click_link "Contact"
+		page.should have_selector 'title', text: full_title('Contact')
+		click_link 'Home'
+		page.should have_selector 'title', text: full_title('')
+		click_link 'Sign up'
+		page.should have_selector 'title', text: full_title('Sign up')
+		click_link 'sample app'
+		page.should have_selector 'title', text: full_title('')
+	end
+end
